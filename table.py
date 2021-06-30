ff4e72afc8dd1a5bd487ccbdafd0d4e2d935d1fd
israel cohen <israel.tsvi.cohen@gmail.com>
2021-06-30 15:14:55 +0300

    added logic in fill function and functions line col and group

--- table.py
+++ table.py
@@ -1,4 +1,5 @@
 import random
+import math
 def TOF():
     n = random.random()%2
     if(n):
@@ -6,28 +7,61 @@ def TOF():
     else :
         return False
 
+def sample_without(number):
+    l = random.sample(range(1, 10), 9)
+    l.pop(l.index(number))
+    return l
 class STable:
     def __init__(self, to_fill):
-        self.table = [[0 for i in range(9)] for j in range(9)] 
+        self.table = [[0 for i in range(9)] for i in range(9)]
         self.tf = to_fill
+        self.fill()
+    
+    def group(self, num):
+        l = []
+        for i in range(3):
+            l.append(self.table[math.floor(num/3)*3][(num%3) * 3 + i])
+        for i in range(3):
+            l.append(self.table[math.floor(num/3)*3 + 1][(num%3) * 3 + i])
+        for i in range(3):
+            l.append(self.table[math.floor(num/3)*3 + 2][(num%3) * 3 + i])
+        return l
+    
+    def col(self, num):
+        l = []
+        for i in range(9):
+            l.append(self.table[i][num])
+        return l
 
+    def line(self, num):
+        return self.table[num]
+         
 
     def fill(self):
         filled = 0
+        listON = random.sample(range(1, 10), 9)
+        for i in range(9):
+            self.table[i][0] = listON[i]
+        l = sample_without(self.table[0][0])
+        for i in range(8):
+            self.table[0][i + 1] = l[i]
         
 
-        
-        
+
+
+    def print(self): 
+        for i in self.table:
+            print(i)
     
 
-s = STable(70)
-s.fill()
-print(s.table)
+
+
         
+s = STable(50)
+s.print()
+print("line number 7:")
+print(s.line(7))
 
 
 
-
-
-
-
+        
